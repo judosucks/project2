c15d3c79f0815aa08297375864e0a213671dadf5
judosucks <judosucks6827@gmail.com>
2024-08-04 17:56:52 +0800

    changes on new syntax

--- src/component/Accordion.js
+++ src/component/Accordion.js
@@ -1,14 +1,28 @@
+import {useState} from "react"
+function Accordion({items}) {
 
-function Accordion({items}){
-  const renderedItem = items.map((item)=>{
-    return(
-        <div key={item.id}>
-            <div>{item.label}</div>
-            <div>{item.content}</div>
-        </div>
+    const [expandedIndex,
+        setExpandedIndex] = useState(0)
+
+    const renderedItem = items.map((item, index) => {
+        const isExpanded = index === expandedIndex //true
+
+        const handleClick =(nextIndex)=>{
+            setExpandedIndex(nextIndex)
+            console.log('expanedIndex is',expandedIndex)
+        }
+        return (
+            <div key={item.id}>
+                <div onClick={()=>handleClick(index)}>{item.label}</div>
+                {isExpanded && <div>{item.content}</div>} 
+                
+            </div>
+            
+        )
+    })
+
+    return (
+        <div>{renderedItem}</div>
     )
-  })
-
- return (<div>{renderedItem}</div>)
 }
 export default Accordion
