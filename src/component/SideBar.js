d069d0d4e4e02dd2603f8e6a47a0e4d94fa2b773
judosucks <judosucks6827@gmail.com>
2024-08-16 04:16:42 +0800

    ''

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -22,11 +22,12 @@ function SideBar() {
             path:'/chatgpt'
         },{
             label:'counter',
-            path:'/counter'
-        },{
-            label:'movie-and-song',
-            path:'/movieandsong'
-        },{
+            path:'/counter'}
+        // },{
+            // label:'movie-and-song',
+            // path:'/movieandsong'
+        // },
+        ,{
             label:'car-page',
             path:'/car-page'
         }
